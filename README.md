f287158eac0c86c0721f7f160b59f7a827f8ae41
hisho <hisho.web@gmail.com>
2021-06-05 22:50:16 +0900

    🎉 init.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# pomodoro
+# Pomodoro Timer App
+
+## commands
+
+### install
+
+```shell
+$ npm ci
+```
+
+### watch when development
+```shell
+$ npm run dev
+```
---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import {VFC} from "react";
+
+const IndexPage: VFC = () => {
+
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+      <main>
+        ああああああ
+      </main>
+    </>
+  )
+}
+
+export default IndexPage;
