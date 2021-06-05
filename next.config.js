f967d1fc266fa9a43c08cae9ef9d8e3eaae1c853
hisho <hisho.web@gmail.com>
2021-06-06 01:15:45 +0900

    🔧 add path alias

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+/* eslint-disable
+   @typescript-eslint/no-var-requires
+*/
+const path = require('path');
 const withPWA = require('next-pwa');
 
 module.exports = withPWA({
+  trailingSlash: true, //https://nextjs.org/docs/api-reference/next.config.js/exportPathMap#adding-a-trailing-slash
+  webpack(config) {
+    config.resolve.alias['@src'] = path.join(__dirname, 'src');
+    return config;
+  },
   pwa: {
     dest: 'public'
   }
