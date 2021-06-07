b7c74e23b24febbfac7d9e22e78fe41c11e2f7bb
hisho <hisho.web@gmail.com>
2021-06-08 00:03:05 +0900

    ✨ add timeline

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,16 @@ import {Timer} from "@src/components";
 import {convertMinutes} from "@src/helpers";
 import {timerType} from "@src/components/type";
 
+const timerState = (timerIndex, currentIndex) => {
+  if (timerIndex === currentIndex) {
+    return 'bg-red-500'
+  } else if (timerIndex > currentIndex) {
+    return 'bg-green-500'
+  } else {
+    return 'bg-gray-300'
+  }
+}
+
 const IndexPage: VFC = () => {
   const timers: timerType[] = [
     {
@@ -36,6 +46,23 @@ const IndexPage: VFC = () => {
 
   return (
     <main className="px-5 w-full max-w-screen-lg mx-auto lg:px-10">
+      <div>
+        <ol className="flex gap-x-2 w-full">
+          {timers.map((item, timerIndex) => (
+            <li
+              className={`relative ${timerState(timerIndex, index)}`}
+              style={{width: `${1 / timers.length * 100}%`}}
+              key={`${item.time}_${timerIndex}`}
+            >
+              <div style={{paddingTop: '100%'}}/>
+              <div className="absolute text-center inset-0 grid place-content-center">
+                <div>{timerIndex + 1}</div>
+                <div>{item.type}</div>
+              </div>
+            </li>
+          ))}
+        </ol>
+      </div>
       <Timer index={index} setIndex={setIndex} timers={timers}/>
     </main>
   )
