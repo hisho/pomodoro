72e55248a7fd8fc9b366e6b4f7d5a04effc7863d
hisho <hisho.web@gmail.com>
2021-06-08 03:07:45 +0900

    🐛 fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // mode: 'jit',
   purge: [
-    '.src/**/*.ts',
-    '.src/**/*.tsx'
+    './src/**/*.ts',
+    './src/**/*.tsx'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
