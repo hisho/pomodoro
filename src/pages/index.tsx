bf918789aba3ac228a08cd5fe47ab7885846d457
hisho <hisho.web@gmail.com>
2021-06-10 04:25:53 +0900

    ♻️ reafactor timers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,33 +3,29 @@ import {TimeLine, Timer} from "@src/components";
 import {convertMinutes} from "@src/helpers";
 import {timerType} from "@src/components/type";
 
+type createTimersType = (
+  props: {
+    work: ReturnType<typeof convertMinutes>
+    break: ReturnType<typeof convertMinutes>
+    longBreak: ReturnType<typeof convertMinutes>
+  }) => timerType[]
+const createTimers: createTimersType = (props) => {
+  return [...Array(8)].map((_, i, array) => {
+    const isWork = i % 2 === 0;
+    const isLast = array.length - 1 === i;
+    return {
+      time: isLast ? props.longBreak : isWork ? props.break : props.break,
+      type: isWork ? 'work' : 'break'
+    }
+  })
+}
+
 const IndexPage: VFC = () => {
-  const timers: timerType[] = [
-    {
-      time: convertMinutes(25),
-      type: 'work'
-    },
-    {
-      time: convertMinutes(5),
-      type: 'break'
-    },
-    {
-      time: convertMinutes(25),
-      type: 'work'
-    },
-    {
-      time: convertMinutes(5),
-      type: 'break'
-    },
-    {
-      time: convertMinutes(25),
-      type: 'work'
-    },
-    {
-      time: convertMinutes(20),
-      type: 'break'
-    },
-  ];
+  const timers = createTimers({
+    work: 25,
+    break: 5,
+    longBreak: 20
+  });
 
   //タイマーの配列のindexを管理するステート
   const [index,setIndex] = useState(0);
