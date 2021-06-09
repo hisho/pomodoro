676af6d5c17e83b68783f9a475a2d49f76737167
hisho <hisho.web@gmail.com>
2021-06-10 03:29:11 +0900

    ♻️ refactor timeline

---
+++ src/components/TimeLine.tsx
@@ -0,0 +1,41 @@
+import {VFC} from "react";
+import {timerType} from "@src/components/type";
+
+const timerState = (timerIndex, currentIndex) => {
+  if (timerIndex === currentIndex) {
+    return 'bg-red-500'
+  } else if (timerIndex > currentIndex) {
+    return 'bg-green-500'
+  } else {
+    return 'bg-gray-300'
+  }
+}
+
+type TimeLinePropsType = {
+  timers: timerType[]
+  timerIndex: number
+}
+
+export const TimeLine:VFC<TimeLinePropsType> = (
+  {
+    timers,
+    timerIndex
+  }) => {
+  return (
+    <ol className="flex gap-x-2 w-full">
+      {timers.map((item, index) => (
+        <li
+          className={`relative ${timerState(index, timerIndex)}`}
+          style={{width: `${1 / timers.length * 100}%`}}
+          key={`${item.time}_${index}`}
+        >
+          <div style={{paddingTop: '100%'}}/>
+          <div className="absolute text-center inset-0 grid place-content-center">
+            <div>{index + 1}</div>
+            <div>{item.type}</div>
+          </div>
+        </li>
+      ))}
+    </ol>
+  )
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export * from '@src/components/Timer';
+export * from '@src/components/TimeLine';
